05af022b09f66c46a0bd74defd8aa5ac873a326b
Ismael_Mae <imaxone333@gmail.com>
2024-09-05 13:13:38 +0100

    Update script.js
    Acabo de añadir linea al comentario.

--- git-and-github-practice/script.js
+++ git-and-github-practice/script.js
@@ -1,5 +1,5 @@
 // JavaScript to be added
 // Cual es la planta favorita de Flalete?
 // La Palmera de Chocolate jajajajja
-//gracias por el chistaco que te conte yo
+//gracias por el chistaco que te conte yo. Si, pero yo ya me lo sabía... Es coña :D
 //pero me mola mas la respuesta de la planta baja
